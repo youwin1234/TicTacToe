cf74a2a45595f18683f0d044875f83bb013a24f3
youwin1234 <tebbytoad@gmail.com>
2019-05-15 18:54:16 -0500

    Updated tictactoe.py
    Added comments and spacing where needed. Also revomed comments and spaces that were not needed.

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,7 @@
+#Author: Tristan Berger
+#Version: 1.02
+#Date: 5/15/19
+
 def CreateBoard(size):
     board = []
     counter = 0
@@ -12,12 +16,13 @@ def CreateBoard(size):
 def PrintBoard(board):
     for i in range(0,len(board)):
         print(board[i])
+
 def PlayerInput(board,player):
     print("PLAYER " + str(player))
     isLegalMove  = False
     while not isLegalMove:
-        pRow = int(input("Enter the row where you'd like to place your symbol (1-" + str(len(board)) + "): ")) - 1
-        pCol = int(input("Enter the column where you'd like to place your symbol (1-" + str(len(board)) + "): ")) - 1
+        pRow = int(input("Enter the row where you'd like to place your symbol (1-" + str(len(board)) + "): ")) -1
+        pCol = int(input("Enter the column where you'd like to place your symbol (1-" + str(len(board)) + "): ")) -1
         if pRow >= 0 and pRow < len(board) and pCol >= 0 and pCol < len(board):
             if board[pRow][pCol] == " ":
                 isLegalMove=True
@@ -29,54 +34,59 @@ def PlayerInput(board,player):
         board[pRow][pCol] = "X"
     else:
         board[pRow][pCol] = "O"
+
+#Checks if there is a winning row placement
 def CheckRow(board, row):
     symbol = board[row][0]
     for i in range(0, len(board)):
-            if board[row][i] == " " or board[row][i] != symbol:
-                return False
+        if board[row][i] == " " or board[row][i] != symbol:
+            return False
     return True
+
+#Checks if there is a winning column placement
 def CheckCol(board, col):
     symbol = board[0][col]
     for i in range(0, len(board)):
-            if board[i][col] == " " or board[i][col] != symbol:
-                return False
+        if board[i][col] == " " or board[i][col] != symbol:
+            return False
     return True
+
+#Checks if there is a winning diagonal placement in the upper left direction
 def CheckDiagUL(board):
     symbol = board[0][0]
     for i in range(0, len(board)):
-            if board[i][i] == " " or board[i][i] != symbol:
-                return False
+        if board[i][i] == " " or board[i][i] != symbol:
+            return False
     return True
+
+#Checks if there is a winning diagonal placement in the upper right direction
 def CheckDiagUR(board):
     symbol = board[0][len(board)-1]
     for i in range(0, len(board)):
-                if board[i][len(board)-(i+1)] == " " or board[i][len(board)-(i+1)] != symbol:
-                    return False
+        if board[i][len(board)-(i+1)] == " " or board[i][len(board)-(i+1)] != symbol:
+            return False
     return True
-#Create the board
+
+#Creates the board
 board = CreateBoard(3)
 isGameWon = False
 currentPlayer = 1
 numMovesRemaining = len(board)**2
 while (not isGameWon) and (numMovesRemaining > 0):
-    #Display the Board
+    #Displays the board
     PrintBoard(board)
-    #Player Input 
+    #Player's Input 
     PlayerInput(board, currentPlayer)
     numMovesRemaining -= 1
     if currentPlayer == 1:
         currentPlayer = 2
     else:
         currentPlayer = 1
-        # Check state
+        #Checks state of game
         for i in range(0, len(board)):
             isGameWon = isGameWon or CheckRow(board, i)
             isGameWon = isGameWon or CheckCol(board, i)
-        #check row
-        #check column
-        #check diagonal (top left)
         isGameWon = isGameWon or CheckDiagUL(board)
-        #check diagonal (top right)
         isGameWon = isGameWon or CheckDiagUR(board)
 #Display end-game content
 if not isGameWon:
